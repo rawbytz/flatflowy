608f38eaa1ef109cd3f63d9262e9fea676537285
rawbytz <rawbytz@gmail.com>
2020-04-16 14:00:43 -0600

    removed hidenotes option

--- FlatFlowy.js
+++ FlatFlowy.js
@@ -1,10 +1,9 @@
-(function flatFlowy_2_9(hideNotes ='_hideNotes_') {
-  if (typeof hideNotes !== "boolean") hideNotes = false;
+(function flatFlowy_2_9() {
   function toastMsg(str, sec, err) {
     WF.showMessage(str.bold(), err);
     setTimeout(() => WF.hideMessage(), (sec || 2) * 1000);
   }
-  const css = `.page.searching .project .name${hideNotes ? ",.page.searching .project .notes" : ""}{display:none!important}.page.searching .project.matches .name.matches${hideNotes ? "" : ",.page.searching .project.matches.noted .name"}{display:block!important}.page.searching .selected>.children>.project .project{margin:0 0 4px!important}.page.searching .children{margin:0!important;padding:0!important;border:0!important}.page.searching .childrenEnd{height:0!important}`;
+  const css = `.page.searching .project .name{display:none!important}.page.searching .project.matches .name.matches,.page.searching .project.matches.noted .name{display:block!important}.page.searching .selected>.children>.project .project{margin:0 0 4px!important}.page.searching .children{margin:0!important;padding:0!important;border:0!important}.page.searching .childrenEnd{height:0!important}`;
   const h = `data:text/css;charset=UTF-8,${encodeURIComponent(css)}`;
   const s = document.querySelector(`link[href="${h}"]`);
   const noSearch = WF.currentSearchQuery() === null;

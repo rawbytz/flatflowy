ad445c2d3ab6696d22e1386a09959e17b029618b
rawbytz <rawbytz@gmail.com>
2020-04-16 14:15:45 -0600

    update css const

--- FlatFlowy.js
+++ FlatFlowy.js
@@ -3,7 +3,7 @@
     WF.showMessage(str.bold(), err);
     setTimeout(() => WF.hideMessage(), (sec || 2) * 1000);
   }
-  const css = `.page.searching .project .name{display:none!important}.page.searching .project.matches .name.matches,.page.searching .project.matches.noted .name{display:block!important}.page.searching .selected>.children>.project .project{margin:0 0 4px!important}.page.searching .children{margin:0!important;padding:0!important;border:0!important}.page.searching .childrenEnd{height:0!important}`;
+  const css = `.page.searching .project .name{display:none}.page.searching .project.matches .name.matches,.page.searching .project.matches.noted .name{display:block}.page.searching .selected>.children>.project .project{margin:0 0 4px}.page.searching .children{margin:0;padding:0;border:0}.page.searching .childrenEnd{height:0}`;
   const h = `data:text/css;charset=UTF-8,${encodeURIComponent(css)}`;
   const s = document.querySelector(`link[href="${h}"]`);
   const noSearch = WF.currentSearchQuery() === null;
@@ -16,4 +16,4 @@
   a.href = h;
   document.head.appendChild(a);
   if (noSearch) toastMsg("Flatflowy: ON");
-})();upd
+})();

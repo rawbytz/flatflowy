c1ea59dfd1828e8a15bff985247165d4288a29cc
rawbytz <rawbytz@gmail.com>
2020-04-16 17:23:12 -0600

    rename to fixFlatClicks

--- FlatFlowy.js
+++ FlatFlowy.js
@@ -4,7 +4,7 @@
     setTimeout(WF.hideMessage, (sec || 2) * 1000);
   }
   // Fix for WorkFlowy bullet click bug. Parent must be visible for animated zoom to work.
-  function flatClicks(e) {
+  function fixFlatClicks(e) {
     if (document.querySelector(".page.searching") && e.target && e.target.parentNode && e.target.parentNode.className.includes("bullet")) {
       location.href = e.target.parentNode.hash;
       e.preventDefault();
@@ -22,6 +22,6 @@
   a.rel = "stylesheet";
   a.href = h;
   document.head.appendChild(a);
-  document.body.addEventListener("click", flatClicks, false);
+  document.body.addEventListener("click", fixFlatClicks, false);
   if (noSearch) toastMsg("Flatflowy: ON");
 })();

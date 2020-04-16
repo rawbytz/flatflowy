d933f18e29afc1392575c0ebd6cd11bdf3e02720
rawbytz <rawbytz@gmail.com>
2020-04-16 15:22:10 -0600

    add metaMatches CSS

--- FlatFlowy.js
+++ FlatFlowy.js
@@ -10,7 +10,7 @@
       e.preventDefault();
     }
   }
-  const css = `.page.searching .project .name{display:none}.page.searching .project.matches .name.matches,.page.searching .project.matches.noted .name{display:block}.page.searching .selected>.children>.project .project{margin:0 0 4px}.page.searching .children{margin:0;padding:0;border:0}.page.searching .childrenEnd{height:0}`;
+  const css = `.page.searching .project .name{display:none}.page.searching .project.matches .name.matches,.page.searching .project.matches.noted .name,.page.searching .project.metaMatches .name{display:block}.page.searching .selected>.children>.project .project{margin:0 0 4px}.page.searching .children{margin:0;padding:0;border:0}.page.searching .childrenEnd{height:0}`;
   const h = `data:text/css;charset=UTF-8,${encodeURIComponent(css)}`;
   const s = document.querySelector(`link[href="${h}"]`);
   const noSearch = WF.currentSearchQuery() === null;

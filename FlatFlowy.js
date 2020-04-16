425fa5d41e93d501ca32c6b350282d542baeb9e8
rawbytz <rawbytz@gmail.com>
2020-04-16 14:18:42 -0600

    update toast

--- FlatFlowy.js
+++ FlatFlowy.js
@@ -1,7 +1,7 @@
 (function flatFlowy_2_9() {
   function toastMsg(str, sec, err) {
-    WF.showMessage(str.bold(), err);
-    setTimeout(() => WF.hideMessage(), (sec || 2) * 1000);
+    WF.showMessage(str, err);
+    setTimeout(WF.hideMessage, (sec || 2) * 1000);
   }
   const css = `.page.searching .project .name{display:none}.page.searching .project.matches .name.matches,.page.searching .project.matches.noted .name{display:block}.page.searching .selected>.children>.project .project{margin:0 0 4px}.page.searching .children{margin:0;padding:0;border:0}.page.searching .childrenEnd{height:0}`;
   const h = `data:text/css;charset=UTF-8,${encodeURIComponent(css)}`;

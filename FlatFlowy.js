fa14aed91c15760d7c4718fc5f2170e2f6416f9f
rawbytz <rawbytz@gmail.com>
2023-08-31 09:48:00 -0600

    added checkmark css to css file

--- FlatFlowy.js
+++ FlatFlowy.js
@@ -10,7 +10,7 @@
       e.preventDefault();
     }
   }
-  const css = `.page.searching .project .name{display:none}.page.searching .project.matches .name.matches,.page.searching .project.matches.noted .name,.page.searching .project.metaMatches .name{display:block}.page.searching .selected>.children>.project .project{margin:0 0 4px}.page.searching .children{margin:0;padding:0;border:0}.newMobileDesign .page.searching .selected .children .children{margin-left:-2px}.newMobileDesign .page.searching .selected .project>.name>.parentArrow{display:none}.newMobileDesign .page.searching .children .content{padding-right:0}`;
+  const css = ``;
   const h = `data:text/css;charset=UTF-8,${encodeURIComponent(css)}`;
   const s = document.querySelector(`link[href="${h}"]`);
   const noSearch = WF.currentSearchQuery() === null;

75e73de5c98c24a7ec8c6052135184bd0011c8fe
rawbytz <rawbytz@gmail.com>
2023-09-01 20:18:26 -0600

    rename no search

--- FlatFlowy.js
+++ FlatFlowy.js
@@ -6,14 +6,14 @@
   const css = `.page.searching .project>.name,.page.searching .project>.notes{height:0;opacity:0}.page.searching .project.matches .name.matches,.page.searching .project.matches.noted .name,.page.searching .project.metaMatches .name{height:100%;opacity:1}.page.searching .children{margin:0;padding:0;border:0}.page.searching .addSiblingButton,.page.searching .expand{display:none}.done .fullMatch .content .contentMatch,.fullMatch .content .contentMatch,.project.metaMatches>.name.with-updates.annotationAdded>.content>.innerContentContainer,.project.metaMatches>.name>.content>.innerContentContainer{background-color:transparent}`;
   const h = `data:text/css;charset=UTF-8,${encodeURIComponent(css)}`;
   const s = document.querySelector(`link[href="${h}"]`);
-  const noSearch = WF.currentSearchQuery() === null;
+  const NO_SEARCH = WF.currentSearchQuery() === null;
   if (s) {
-    if (noSearch) toastMsg(`FlatFlowy: ${s.disabled ? "ON" : "OFF"}`);
+    if (NO_SEARCH) toastMsg(`FlatFlowy: ${s.disabled ? "ON" : "OFF"}`);
     return void (s.disabled = !s.disabled);
   }
   const a = document.createElement("link");
   a.rel = "stylesheet";
   a.href = h;
   document.head.appendChild(a);
-  if (noSearch) toastMsg("Flatflowy: ON");
+  if (NO_SEARCH) toastMsg("Flatflowy: ON");
 })();
